75fbbafffea1481379541b3557bb307c43dc1e4a
Royer Reyes León <royerleon7@gmail.com>
2022-06-17 22:01:58 -0500

    navegación entre páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    /* {
+      path: '',
+      loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    }, */
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inicio',
+        loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
+    },
+    {
+        path: 'alert',
+        loadChildren: () => import('./pages/alert/alert.module').then(m => m.AlertPageModule)
+    },
+    {
+        path: 'action-sheet',
+        loadChildren: () => import('./pages/action-sheet/action-sheet.module').then(m => m.ActionSheetPageModule)
+    }
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
